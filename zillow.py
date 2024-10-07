f35c3a2b7fbced4979c870cbc413a9d13cdf7b62
Dipesh Poudel <43557135+Dpoudel18@users.noreply.github.com>
2024-10-07 03:24:26 -0400

    zillow standardization

--- zillow.py
+++ zillow.py
@@ -1,111 +1,88 @@
-# pip3 install lxml
-# pip3 install requests
-# pip3 install beautifulsoup4
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import json
 import time 
 
-header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-        ,'referer':'https://www.zillow.com/pittsburgh-pa/rentals/'
-          }
+# Define the headers to mimic a browser request
+header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+    'referer': 'https://www.zillow.com/pittsburgh-pa/rentals/'
+}
 
+# Function to retrieve property URLs from Zillow rental listings
 def get_url():
+    print("Collecting Zillow Property URL for Scraping.....")
     property_url = []
-    for page in range(2):
+    # Loop through the first twelve pages of rental listings
+    for page in range(1,21):
+        # Make a request to the Zillow rental listings page
         data = requests.get(f'https://www.zillow.com/pittsburgh-pa/rentals/{page}_p', headers=header)
         soup = BeautifulSoup(data.text, "lxml")
+        
+        # Find all property card links
         property_links = soup.find_all('a', {'data-test': 'property-card-link'})
         for result in property_links:
-            property_url.append("zillow.com" + result.get('href'))  
+            # Append each property's URL to the list
+            link = result.get('href')
+            if 'zillow.com' not in link:
+                url_val = "zillow.com" + result.get('href')
+            else:
+                url_val = result.get('href')
+            if url_val not in property_url:
+                property_url.append(url_val)  
+    print("\nCollected Zillow Property URL for Scraping! Reading for Scraping!\n")
     return property_url
 
+# Function to scrape detailed data for each property
 def zillow_data():
+    all_info = []  # List to hold all property information
+    property_url = list(set(get_url()))  # Get the list of property URLs
 
-    all_info = []
-    property_url = get_url()
-
+    print("\nZillow Data Extraction Started!\n")
     for url_link in property_url:
         try:
+            # Make a request to each property URL
+            
             data = requests.get(f'https://{url_link}', headers=header)
-        except requests.exceptions.ConnectionError:
-            print("Site not rechable", url)
-        print(f'https://{url_link}')
+            print(f'Extracted... https://{url_link}')
+        except requests.exceptions.ConnectionError as e:
+            print("Site not reachable! Zillow Rate Limit Reached!", url_link)
+            time.sleep(3)
+            continue  # Skip to the next URL on connection error
+        
+        # Print the current URL being processed
         soup = BeautifulSoup(data.text, "lxml")
 
-        # Find the JSON-LD script tag
+        # Find the JSON-LD script tag containing the property data
         json_ld = soup.find('script', type='application/ld+json')
 
-        # Load the JSON data
+        # Load the JSON data if available
         if json_ld:
             json_data = json.loads(json_ld.string)
 
-        name = json_data['name']
-        address = json_data['address']['streetAddress'] + ", " + json_data['address']['addressLocality'] + ", " + json_data['address']['addressRegion'] + ", " + json_data['address']['postalCode']
-        zip_code = json_data['address']['postalCode']
-        price = json_data['offers']['lowPrice']
-        latitude = json_data['geo']['latitude']
-        longitute = json_data['geo']['longitude']
-        phone = json_data['telephone']
-        description = json_data['description']
-        amenity = json_data['amenityFeature']['name']
-        image = json_data['image']
-        url = 'https://zillow.com' + json_data['id']
-        # name, address, zip_code, price, latitude, longitute, phone, description, amenity, image, url
-        all_info.append([name, address, zip_code, price, latitude, longitute, phone, description, amenity, image, url])
-        time.sleep(1)
-
-    df = pd.DataFrame(all_info, columns = ["Name", "Address", "Zip_code", "Price", "Latitude", "Longitute", "Phone", "Description", "Amenity", "Image", "Url"]) 
+            # Extract relevant information from the JSON data
+            name = json_data['name']
+            address = (json_data['address']['streetAddress'] + ", " +
+                       json_data['address']['addressLocality'] + ", " +
+                       json_data['address']['addressRegion'] + ", " +
+                       json_data['address']['postalCode'])
+            zip_code = json_data['address']['postalCode']
+            price = json_data['offers']['lowPrice']
+            latitude = json_data['geo']['latitude']
+            longitude = json_data['geo']['longitude']
+            phone = json_data.get('telephone', 'N/A')  
+            description = json_data['description']
+            amenity = json_data['amenityFeature']['name']
+            image = json_data['image']
+            url = 'https://zillow.com' + json_data['id']
+            data_source = 'Zillow'
+            all_info.append([name, address, zip_code, price, latitude, longitude, phone, description, amenity, image, url, data_source])
+            time.sleep(1)
+    print("\nZillow Data Extraction Completed!\n")
+    # Create a DataFrame from the collected data
+    df = pd.DataFrame(all_info, columns=["name", "location", "zip_code", "price", "latitude", "longitude", "phone", "description", "amenity", "image_url", "property_url", "data_source"])
+    df.to_csv("data/cleaned_zillow_data.csv")
     return df
 
-# def zillow_data():
-#     property_name = []
-#     addr = []
-#     pr = []
-#     property_url = []
-#     bed = []
-
-#     for page in range(2):
-#         data = requests.get(f'https://www.zillow.com/pittsburgh-pa/rentals/{page}_p', headers=header)
-#         soup = BeautifulSoup(data.text, "lxml")
-#         address = soup.find_all('address', {'data-test':'property-card-addr'})
-#         price = soup.find_all('span', {'data-test':'property-card-price'})
-#         property_links = soup.find_all('a', {'data-test': 'property-card-link'})
-#         try:
-#             beds = soup.find_all('span', {'class' : 'Text-c11n-8-102-0__sc-aiai24-0 PropertyCardInventoryBox__BedText-srp-8-102-0__sc-1jotqb7-2 bhZlvi hZrLIt'})
-#         except:
-#             beds = ""
-
-#         for result in address:
-#             try:
-#                 if "|" in result.text:
-#                     property_name.append(result.text.split("|")[0])
-#                 else:
-#                     property_name.append("")
-#                 addr.append(result.text.split("|")[1])
-#             except:
-#                 addr.append(result.text)
-
-#         for result in price:
-#             pr.append(result.text)   
-
-#         for result in property_links:
-#             property_url.append("zillow.com" + result.get('href'))   
-
-#         for i, result in enumerate(beds):
-#             try:
-#                 bed.append(result.text)   
-#             except:
-#                 bed.append("")
-
-#     big_row = list(zip(property_name, addr, pr, property_url))
-
-#     df = pd.DataFrame(big_row, columns = ['Property Name', 'Address', 'Price', 'Property Link']) 
-
-#     df.to_csv("demo.csv", index=False)
-
-#     return df
-
-# print(zillow_data())
+print(zillow_data())
